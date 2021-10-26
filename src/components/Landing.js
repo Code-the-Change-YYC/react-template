ad1e850cc22b4b3a51a4eab5199bde1893294f42
Jofred <jjay.cayabyab@gmail.com>
2021-10-25 21:50:06 -0600

    Add list of included React libraries

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,23 +3,18 @@ import React from "react";
 const Landing = () => (
   <>
     <p>
-      Welcome to Hack the Change 2021!
+      Welcome to Hack the Change 2021! Here's a handy boilerplate that handles most project setup you might need.
+      Here are things that are included:
     </p>
+    <ul>
+      <li>Material UI</li>
+      <li>React Router</li>
+      <li>Redux</li>
+    </ul>
     <p>
       This boilerplate site was bootstrapped with Create React App.
     </p>
-    <List />
   </>
 )
-
-const List = () => (
-  <div>
-    <p>Hello! Here's a to-do:</p>
-    <ul>
-      <li>Handle back-end REST API</li>
-      <li>Create simple template</li>
-    </ul>
-  </div>
-);
 
 export default Landing;
